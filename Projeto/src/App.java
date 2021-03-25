679932a56ed48e24997c0e5586fb315ea5c6d0df
antoniobarolo <antoniobarolog@gmail.com>
2021-03-24 23:10:38 -0300

    Update App.java

--- Projeto/src/App.java
+++ Projeto/src/App.java
@@ -4,7 +4,6 @@ import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) throws Exception {
 
-<<<<<<< HEAD
 		String resp;
 		do {
 			resp = JOptionPane.showInputDialog("Escolha uma operação\n1. Cadastrar Empregado\n2. Cadastrar Cliente\n3. Pesquisar\n4. Listar Empregados\n5. Listar Clientes\n6.Remover\n7.Finalizar");
@@ -12,7 +11,6 @@ public class App {
             case "1":
             Object[] options = {"Gerente", "Vendedor"};
                     JOptionPane.showOptionDialog(null, "Cadastro de gerente ou vendedor?", "Cadastrar Empregado", JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,null,options, options[0]);
-                    
 				break;
 			case "2":
 				break;
@@ -30,30 +28,6 @@ public class App {
 				JOptionPane.showMessageDialog(null, "Opção inválida. Se informe!");
 			}
 		} while (resp.equals("1") || resp.equals("2") || resp.equals("3") || resp.equals("4")|| resp.equals("5") ||  resp.equals("6")|| resp.equals("7")|| resp.contentEquals(null));
-=======
-        String resp;
-        do {
-            resp = JOptionPane.showInputDialog("oi");
-            switch (resp) {
-            case "1":
-                JOptionPane.showMessageDialog(null, "Tá tudo lotado!", "Eita!", JOptionPane.WARNING_MESSAGE);
-                break;
-            case "2":
-                break;
-            case "3":
-                break;
-            case "4":
-                break;
-            case "5":
-                break;
-            case "6":
-                break;
-            default:
-                JOptionPane.showMessageDialog(null, "Opção inválida. Se informe!");
-            }
-        } while (resp.equals("1") || resp.equals("2") || resp.equals("3") || resp.equals("4") || resp.equals("5")
-                || resp.equals("6") || resp.contentEquals(null));
->>>>>>> 05adc3127717e013ccd4faa77dc87c1ef8482ec1
 
     }
 
