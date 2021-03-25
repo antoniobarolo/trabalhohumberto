648fbd13090abcb9bccb823d65e34ed73375a7f1
Aranaaa <andre.arana.deziderio@gmail.com>
2021-03-24 23:07:10 -0300

    Update App.java

--- Projeto/src/App.java
+++ Projeto/src/App.java
@@ -6,10 +6,12 @@ public class App {
 
 		String resp;
 		do {
-			resp = JOptionPane.showInputDialog("oi");
+			resp = JOptionPane.showInputDialog("Escolha uma operação\n1. Cadastrar Empregado\n2. Cadastrar Cliente\n3. Pesquisar\n4. Listar Empregados\n5. Listar Clientes\n6.Remover\n7.Finalizar");
 			switch (resp) {
-			case "1":
-					JOptionPane.showMessageDialog(null, "Tá tudo lotado!", "Eita!", JOptionPane.WARNING_MESSAGE);
+            case "1":
+            Object[] options = {"Gerente", "Vendedor"};
+                    JOptionPane.showOptionDialog(null, "Cadastro de gerente ou vendedor?", "Cadastrar Empregado", JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,null,options, options[0]);
+                    
 				break;
 			case "2":
 				break;
@@ -20,11 +22,13 @@ public class App {
 			case "5":
 				break;
 			case "6":
+                break;
+            case "7":
 				break;
 			default:
 				JOptionPane.showMessageDialog(null, "Opção inválida. Se informe!");
 			}
-		} while (resp.equals("1") || resp.equals("2") || resp.equals("3") || resp.equals("4")|| resp.equals("5") ||  resp.equals("6")|| resp.contentEquals(null));
+		} while (resp.equals("1") || resp.equals("2") || resp.equals("3") || resp.equals("4")|| resp.equals("5") ||  resp.equals("6")|| resp.equals("7")|| resp.contentEquals(null));
 
     }
 }
