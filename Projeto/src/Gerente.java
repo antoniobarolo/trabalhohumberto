d957e8c8e32a3d494740cb6860f90232a8033ec9
Aranaaa <andre.arana.deziderio@gmail.com>
2021-03-24 21:32:11 -0300

    Create Gerente.java

---
+++ Projeto/src/Gerente.java
@@ -0,0 +1,9 @@
+public class Gerente extends Empregado {
+    private double salario;
+    private double bonus;
+
+    public void setVendedor(double salario, double bonus){
+        this.salario = salario;
+        this.bonus = bonus;
+    }
+}
