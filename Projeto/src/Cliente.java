03e11b9693c80f6026ee4b61b767d2999b36c152
Aranaaa <andre.arana.deziderio@gmail.com>
2021-03-24 21:26:06 -0300

    Create Cliente.java

---
+++ Projeto/src/Cliente.java
@@ -0,0 +1,8 @@
+public class Cliente extends Pessoa{
+    private double valorDaDivida;
+
+    public void setCliente(double valorDaDivida){
+        this.valorDaDivida = valorDaDivida;
+        
+    }
+}
