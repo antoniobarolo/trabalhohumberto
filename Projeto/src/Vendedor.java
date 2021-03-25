7ea43208645716f9dcfb5707acb63356534d7247
antoniobarolo <antoniobarolog@gmail.com>
2021-03-24 21:58:46 -0300

    o

--- Projeto/src/Vendedor.java
+++ Projeto/src/Vendedor.java
@@ -1,9 +1,13 @@
-public class Vendedor extends Empregado {
+public class Vendedor extends Empregado implements Salario{
     private double totalDasVendas;
     private double comissao;
 
     public void setVendedor(double totalDasVendas, double comissao){
         this.totalDasVendas = totalDasVendas;
         this.comissao = comissao;
+    }
+
+    public double CalcularSalario(){
+        return this.totalDasVendas*this.comissao;
     }
 }
