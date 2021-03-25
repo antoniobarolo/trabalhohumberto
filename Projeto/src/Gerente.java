c6fa10182568a4a908e0f4d8ae0f1cc2d0399a62
antoniobarolo <antoniobarolog@gmail.com>
2021-03-24 21:55:49 -0300

    o

--- Projeto/src/Gerente.java
+++ Projeto/src/Gerente.java
@@ -1,9 +1,14 @@
-public class Gerente extends Empregado {
+public class Gerente extends Empregado implements Bonus {
     private double salario;
     private double bonus;
 
     public void setVendedor(double salario, double bonus){
         this.salario = salario;
         this.bonus = bonus;
+    }
+    
+    public double calcularBonus() {
+        double bonus = this.salario*this.bonus;
+        return bonus;
     }
 }
