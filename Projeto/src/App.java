efd2e142ef100167e016c98a632a92908eb905b4
antoniobarolo <antoniobarolog@gmail.com>
2021-03-24 21:15:48 -0300

    o

--- Projeto/src/App.java
+++ Projeto/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+
+        Pessoa jorge = new Pessoa();
+        jorge.setPessoa("Jorge", "12234");
+       System.out.println(jorge.toString());
     }
 }
---
+++ Projeto/src/Empregado.java
@@ -0,0 +1,7 @@
+public class Empregado extends Pessoa {
+    protected String matricula;
+
+    public Empregado(String matricula) {
+        this.matricula = matricula;
+    }
+}
